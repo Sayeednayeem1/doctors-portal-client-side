400fc6d53bd1fc5e01139bee18166e4fb4fce428
Abu Sayeed <sayeed@pop-os.localdomain>
2023-03-11 22:40:04 +0600

    added services component

--- src/pages/Home/Services/Service.js
+++ src/pages/Home/Services/Service.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const Service = () => {
+const Service = ({service}) => {
+    const {name, details, icon} = service;
     return (
         <div>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card bg-base-100 shadow-xl">
                 <figure className="px-10 pt-10">
-                    <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                    <img src={icon} alt="Shoes" className="rounded-xl" />
                 </figure>
                 <div className="card-body items-center text-center">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
+                    <h2 className="card-title">{name}</h2>
+                    <p>{details}</p>
                 </div>
             </div>
         </div>
--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import fluoride from "../../../assets/images/fluoride.png";
+import cavity from "../../../assets/images/cavity.png";
+import whitening from "../../../assets/images/whitening.png";
+import Service from './Service';
 
 const Services = () => {
+    const servicesData = [
+        {
+            id: 1,
+            name: "Fluoride Treatment",
+            details: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            icon: fluoride,
+            bgClass: "bg-gradient-to-r from-primary to-secondary"
+        },
+        {
+            id: 2,
+            name: "Cavity Filling",
+            details: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            icon: cavity,
+            bgClass: "bg-accent"
+        },
+        {
+            id: 3,
+            name: "Teeth WHitening",
+            details: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            icon: whitening,
+            bgClass: "bg-gradient-to-r from-primary to-secondary"
+        }
+    ]
     return (
-        <div>
-            <h3>this is a test</h3>
+        <div className='mt-16'>
+            <div className='text-center'>
+                <h4 className='text-2xl font-bold text-secondary '>OUR SERVICES</h4>
+                <h5 className='text-3xl'>Services we provide</h5>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8'>
+                {
+                    servicesData.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
         </div>
     );
 };
