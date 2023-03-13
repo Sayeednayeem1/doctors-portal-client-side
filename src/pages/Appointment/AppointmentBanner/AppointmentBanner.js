7bafb9974895cc4f0c686827e9c14cf3d9b7cf2d
Abu Sayeed <sayeed@pop-os.localdomain>
2023-03-13 19:27:39 +0600

    added AvailableAppointments component to the Appointment route

--- src/pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import banner from '../../../assets/images/chair.png';
+import { DayPicker } from 'react-day-picker';
 
-const AppointmentBanner = () => {
+const AppointmentBanner = ({ selectedDate, setSelectedDate }) => {
     return (
-        <div>
-            <div className="hero ">
+        <div className='my-6'>
+            <div className="hero">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src={banner} className="lg:w-1/2 rounded-lg shadow-2xl" alt='' />
-                    <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                    <img src={banner} className="md:w-1/2 rounded-lg shadow-2xl" alt='' />
+                    <div className='mr-8'>
+                        <DayPicker
+
+                            mode='single'
+                            selected={selectedDate}
+                            onSelect={setSelectedDate}
+
+                        />
                     </div>
                 </div>
             </div>
