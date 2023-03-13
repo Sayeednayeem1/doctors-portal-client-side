783836641b20e96da4ea129c4713ebc8092d427b
Abu Sayeed <sayeed@pop-os.localdomain>
2023-03-13 23:28:07 +0600

    added input and textarea in the BookingModal component

--- src/pages/Appointment/BookingModal/BookingModal.js
+++ src/pages/Appointment/BookingModal/BookingModal.js
@@ -1,7 +1,8 @@
+import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({treatment}) => {
-    let {name} = treatment;
+const BookingModal = ({ treatment, selectedDate }) => {
+    let { name, slots } = treatment;
     return (
         <div>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -9,7 +10,18 @@ const BookingModal = ({treatment}) => {
                 <div className="modal-box relative">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">{name}</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <form className='mt-8 grid grid-cols-1 gap-6'>
+                        <input type="text" value={format(selectedDate, "PP")} disabled className="input input-bordered w-full" />
+                        <select className="select select-bordered w-full">
+                            {
+                                slots.map(slot => <option selected value={slot}>{slot}</option>)
+                            }
+                        </select>
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+                        <br />
+                        <input className='btn btn-primary w-full' type="submit" value='submit' />
+                    </form>
                 </div>
             </div>
         </div>
