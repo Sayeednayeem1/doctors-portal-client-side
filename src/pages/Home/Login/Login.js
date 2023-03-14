c814646fd2996104d1dff55e936f491f49322a1a
Abu Sayeed <sayeed@pop-os.localdomain>
2023-03-14 11:24:27 +0600

    installed react hook form

--- src/pages/Home/Login/Login.js
+++ src/pages/Home/Login/Login.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            <h1>This is login</h1>
+        <div className='h-[800px] flex justify-center items-center'>
+            <div>
+                <h1>This is login</h1>
+            </div>
         </div>
     );
 };
